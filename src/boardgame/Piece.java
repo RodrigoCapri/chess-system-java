ac3ed4ff87cf824a35db81872caeeed2c3a603da
RodrigoCapri <rodrigocamr90@gmail.com>
2024-01-02 17:26:49 -0300

    Começando a implementar Board e Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public class Piece {
+	
+	//Protected: somente classes e subclasses do mesmo pacote vão poder acessar o atributo
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+	}
+
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+}
